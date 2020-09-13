e417acc3c689ecabd34f93ed36084a58be9bd082
Matheus Silva <msilva.jus@gmail.com>
2020-09-12 23:34:19 -0300

    Init commit with my project to study to Oracle Certified Associate Java Programmer.

---
+++ src/main/java/com/msilvadev/aplicacaojavaexecutavel/HelloWorld.java
@@ -0,0 +1,21 @@
+package com.msilvadev.aplicacaojavaexecutavel;
+
+/**
+ * @author msilvadev
+ */
+public class HelloWorld {
+
+  public static void main(String... args){
+    System.out.println("Program Running...");
+
+    System.out.println("Properties -> " + HelloWorld.getPropertyValue());
+
+    for(int i=0; i < args.length; i++){
+      System.out.println(String.format("Array[%s] - value %s", i, args[i]));
+    }
+  }
+
+  private static String getPropertyValue(){
+    return System.getProperty("profile");
+  }
+}
