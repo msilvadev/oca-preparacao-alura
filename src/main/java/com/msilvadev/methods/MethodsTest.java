f1f2e969344fc08e79a6544fddc93f17d14cad58
msilvadev <msilva.jus@gmail.com>
2021-02-15 20:04:30 -0300

    methods, tests with string

---
+++ src/main/java/com/msilvadev/methods/MethodsTest.java
@@ -0,0 +1,25 @@
+package com.msilvadev.methods;
+
+public class MethodsTest {
+
+    public static void main(String  [ ] args){
+
+    }
+}
+
+class ClassWithMethods{
+
+    int getNumber() {
+        return 5;
+    }
+
+    /**
+     * Not works because we need cover all possibilities with return.
+     * @param i
+     * @return
+     */
+    String getText(int i) {
+        if(i > 0) return "greater";
+        else if(i < 0) return "less";
+    }
+}
---
+++ src/main/java/com/msilvadev/string/StringTest.java
@@ -0,0 +1,9 @@
+package com.msilvadev.string;
+
+public class StringTest {
+
+    public static void main(String[] args) {
+        String String = "teste";
+        System.out.println(String);
+    }
+}
